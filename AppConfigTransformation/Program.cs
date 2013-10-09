b8b258de6e7331a2e28d297d79a369a81468b670
Andrew <luzkovskyandrey@gmail.com>
2013-10-09 22:30:58 +0300

    init commit

---
+++ AppConfigTransformation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace AppConfigTransformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("connection={0} ", ConfigurationManager.ConnectionStrings["SQL-ConnectionString"].ConnectionString);
+            Console.WriteLine("app settings={0} ", ConfigurationManager.AppSettings["rabbitMQUrl"]);
+            Console.ReadLine();
+        }
+    }
+}
